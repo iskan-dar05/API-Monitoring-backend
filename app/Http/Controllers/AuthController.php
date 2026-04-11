426227059ab32cd2ba568fd45ab97163bdc68d93
iskandar boukhenoufa <iskandar@iskandar.iskandar@skou.com>
2026-04-11 15:49:42 -0400

    fix passport issue

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -12,32 +12,73 @@ use Illuminate\Support\Facades\Validator;
 class AuthController extends Controller
 {
     public function register(Request $request){
-	$request->validate([
-		"name"=>"required|min:4",
-		"email"=>"required|email",
-		"password"=>"required|min:8"
-	]);
+		$request->validate([
+			"name"=>"required|min:4",
+			"email"=>"required|email",
+			"password"=>"required|min:8"
+		]);
 
-	$dt = Carbon::now();
-	$join_date = $dt->toDayDateTimeString();
-	$user = new User();
-	$user->name = $request->name;
-	$user->email = $request->email;
-	$user->password = Hash::make($request->password);
-	$user->save();
+		$dt = Carbon::now();
+		$join_date = $dt->toDayDateTimeString();
+		$user = new User();
+		$user->name = $request->name;
+		$user->email = $request->email;
+		$user->password = Hash::make($request->password);
+		$user->save();
 
-	$data = [];
-	$data["response_code"] = '200';
-	$data["status"] = 'success';
-	$data["message"] = 'register success';
-	return response()->json($data);
+		$data = [];
+		$data["response_code"] = '200';
+		$data["status"] = 'success';
+		$data["message"] = 'register success';
+		return response()->json($data);
 }
 
     public function login(Request $request){
-	$request->validate([
-		"email"=>"required|string",
-		"password"=>"required|string"
-	]);
+		$request->validate([
+			"email"=>"required|string",
+			"password"=>"required|string"
+		]);
+
+		try{
+			$email = $request->email;
+			$password = $request->password;
+
+
+			if(Auth::attempt(['email' => $email, 'password' => $password]))
+			{
+				$user = Auth::User();
+				$accessToken = $user->createToken($user->email)->accessToken;
+
+				$data = []
+
+				$data['response_code'] = '200';
+				$data['status'] = 'success';
+				$data['message'] = 'success Login';
+				$data['user_infor'] = $user;
+				$data['token'] = $accessToken;
+
+				return response()->json($data);
+			}else{
+				$data = [];
+				$data['response_code'] = '401';
+				$data['status'] = 'error';
+				$data['message'] = 'fail Login';
+				return response()->json($data);
+			}
+		}catch(\Exception $e){
+			\Log::info($e);
+			$data = [];
+			$data['response_code'] = '401';
+			$data['status'] = 'error';
+			$data['message'] = 'fail Login';
+
+			return response()->json($data);
+		}
+
+	}
+
+
+
 
 	
 
